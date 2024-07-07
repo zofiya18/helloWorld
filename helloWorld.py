237ed87dc4e2437b514f7634fd9e3a5cb0b68eba
zofiya18 <zofiya18@gmail.com>
2024-07-07 20:27:10 +0300

    helloWorld

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def add_text_area():
+    st.session_state.btn = str(st.session_state.btn)+" Hello World!\n"
+    return
+
+st.text_area('btn1', key='btn',height=3, label_visibility="hidden")
+st.button('click here', on_click=add_text_area)
